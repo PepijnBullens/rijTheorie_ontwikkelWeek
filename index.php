fa7c54a92faaab4e6510415d22674335bee4f85e
PepijnBullens <pepijnpersoonlijk@gmail.com>
2024-05-27 12:07:50 +0200

    started working on displaying questions from a database.

--- index.php
+++ index.php
@@ -1,10 +1,42 @@
 <?php
     include_once './db_connect.php';
 
-    if(isset($_GET["categorie"]) && isset($_GET["type"])) {
-        $type = $_GET["type"];
-        $category = $_GET["category"];
+    $hazardRecognition = [
+        "count" => 26,
+        "type" => 4,
+        "options" => [
+            "Brake",
+            "Release gas",
+            "Nothing"
+        ]
+    ];
 
-        $stmt = $con->prepare("SELECT");
+    $currentType = $hazardRecognition;
+
+    $stmt = $con->prepare("SELECT id, image, question FROM questions WHERE type = ? ORDER BY RAND() LIMIT ?;");
+    $stmt->bind_param("ii", $currentType["type"], $currentType["count"]);
+    $stmt->execute();
+
+    $result = $stmt->get_result();
+    if($result->num_rows > 0) {
+        while($row = $result->fetch_assoc()) {
+?>
+
+<div class="question">
+    <img src="./img/<?= $row["image"] ?>" alt="image">
+    <p><?= $row["question"] ?></p>
+    <?php
+        foreach($currentType["options"] as $option) {
+    ?>
+
+    <button><?= $option ?></button>
+
+    <?php
+        }
+    ?>
+</div>
+
+<?php
+        }
     }
 ?>
