e52c5c142d136d4f2c81f87e1b6305aa3683741b
Tjardo <tjardo.antonie@gmail.com>
2024-05-27 10:55:16 +0200

    added initial front end code.

--- index.php
+++ index.php
@@ -1,10 +1,44 @@
-<?php
-    include_once './db_connect.php';
+<!DOCTYPE html>
+<html lang="en">
 
-    if(isset($_GET["categorie"]) && isset($_GET["type"])) {
-        $type = $_GET["type"];
-        $category = $_GET["category"];
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Gevaren herkenning</title>
+    <link href="style.css" rel="stylesheet">
+</head>
 
-        $stmt = $con->prepare("SELECT");
-    }
-?>
+<body>
+    <header>
+        <div id="timer">
+            <p id="timer_text">1</p>
+        </div>
+    </header>
+
+    <div id="content">
+        <div id="section_pic">
+            <img src="https://placehold.co/600x400">
+        </div>
+        <div id="section_answers">
+
+            <div class="answer_options">
+                <p class="answer_text">Remmen</p>
+            </div>
+
+            <div class="answer_options">
+                <p class="answer_text">Gas loslaten</p>
+            </div>
+
+            <div class="answer_options">
+                <p class="answer_text">Niets</p>
+            </div>
+
+        </div>
+    </div>
+
+    <footer>
+        <p id="footer_text">Vraag 1 van 20</p>
+    </footer>
+</body>
+
+</html>
