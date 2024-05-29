aa795d305dd4c0847262d8bfa1cf2a84bae47e0a
Tjardo <tjardo.antonie@gmail.com>
2024-05-29 15:38:42 +0200

    making start screen

---
+++ assets/php/start.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Start</title>
+    <link href="../css/start.css" rel="stylesheet">
+</head>
+
+<body>
+    <header>
+        <img src="../images/logo.png">
+    </header>
+
+    <div id="content">
+        <div id="section_pic">
+            <img src="https://placehold.co/600x400">
+        </div>
+        <div id="section_button">
+
+            <div onclick="select(this, 0);" id="start_button">
+                <p class="answer_text">Start test</p>
+            </div>
+        </div>
+    </div>
+    <footer></footer>
+
+</body>
+
+</html>
